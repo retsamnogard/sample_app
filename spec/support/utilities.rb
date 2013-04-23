7682b2bc18774f7486e25664265d8b87aaa6d36f
Felix Anderson <felixsamanderson@gmail.com>
2013-04-23 03:27:06 -0700

    Chapter 8 exercises

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -11,3 +11,9 @@ RSpec::Matchers.define :have_error_message do |message|
     page.should have_selector('div.alert.alert-error', text: message)
   end
 end
+
+RSpec::Matchers.define :have_title do |title|
+  match do |page|
+    page.should have_selector('title', text: title)
+  end
+end
